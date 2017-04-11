192119a55f3781065894115148b772273fb20579
Meirion Hughes <mhughes@geologging.com>
2017-04-11 15:19:27 +0100

    example: service registration

--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
 import { Server } from 'ws';
-import { Viae, ViaContext } from './src/index';
-
-
+import { Viae, ViaContext, Wire } from './src/index';
 
 let wss = new Server({ port: 9090 });
 let server = new Viae(wss);
@@ -30,11 +28,41 @@ server.route({
   handlers: [(ctx: ViaContext) => ctx.sendStatus(200)]
 });
 
+var service: Wire;
+
+server.route({
+  method: "REGISTER",
+  path: "/service",
+  handlers: [
+    (ctx: ViaContext) => {
+      console.log("registered");
+      service = ctx.wire;
+      var dispose = () => service = undefined;
+      ctx.wire.on("close", () => dispose());
+
+      return ctx.sendStatus(200);
+    }]
+});
+
 server.route({
   method: "GET",
-  path: "/exception",
-  handlers: [(ctx: ViaContext) => { throw Error("something nasty happened"); }]
+  path: "/service",
+  handlers: [
+    async (ctx: ViaContext) => {
+      if (service == undefined) { return ctx.sendStatus(404); }
+
+      let result = await server.request({
+        method: "GET",
+        path: "/service",
+        body: ctx.req.body
+      }, false, service);
+
+      console.log(result);
+
+      return ctx.sendStatus(result.res.status, result.res.body);
+    }]
 });
+
 
 server.use((ctx) => {
   return 404;
@@ -44,7 +72,8 @@ server.use((ctx, err) => {
   if (typeof (err) == "number") {
     ctx.res.status = err;
   } else {
-    ctx.res.body = JSON.stringify(err, Object.getOwnPropertyNames(err))
+    console.log(err);
+    ctx.res.body = err;
     ctx.res.status = 504;
   }
   return ctx.send();
@@ -54,10 +83,11 @@ server.before((ctx: ViaContext) => {
   ctx["_start"] = process.hrtime();
 });
 
-server.after((ctx: ViaContext) => {
+server.after((_) => (_.req) ? undefined : false, (ctx: ViaContext) => {
   let start = ctx["_start"];
   let hrspan = process.hrtime(start);
   let span = hrspan[0] * 1000 + hrspan[1] / 1000000;
+
   console.log(`${ctx.req.method} ${ctx.req.path} - ${ctx.res.status} (${span}ms)`);
 });
 
