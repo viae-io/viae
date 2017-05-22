2635130d0461b14a900c42011d986f8db38e4e8a
Meirion Hughes <mhughes@geologging.com>
2017-05-22 12:02:02 +0100

    feat: improve wire interface

--- src/wire.ts
+++ src/wire.ts
@@ -1,14 +1,18 @@
 /**
- * A basic interface defining a binary bi-directional communication wire. 
+ * A basic interface defining a binary bi-directional communication wire.  
  */
 export interface Wire {
   send(data: ArrayBuffer): void;
-  close(dispose?: boolean);
+  close();
 
   on(event: "message", cb: (data: ArrayBuffer) => void): void;
-  on(event: "close", cb: (disposed?: boolean) => void): void;
-  on(event: "error", cb: (err: any) => void): void;
+
   on(event: "open", cb: () => void): void;
+  on(event: "closing", cb: () => void): void;
+  on(event: "close", cb: () => void): void;
+  on(event: "error", cb: (err: any) => void): void;
+
+  state: "opening" | "open" | "closing" | "closed";
 
   [index: string]: any;
 }
