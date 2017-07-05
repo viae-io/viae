3e34cf126ea8c3bf44839130accebe1bd3c1e1e4
Meirion Hughes <mhughes@geologging.com>
2017-07-05 09:23:16 +0100

    refactor: scribe plugin as class

--- server.ts
+++ server.ts
@@ -2,11 +2,13 @@ import "core-js/modules/es7.symbol.async-iterator";
 
 import { Server as WebSocketServer } from 'ws';
 import { Viae, RequestContext, Method, Router } from './src/index';
-import { scribe, unhandled, iterable } from './src';
+import { Scribe, unhandled, iterable } from './src';
 
 let wss = new WebSocketServer({ port: 9090 });
 let server = new Viae(wss);
 let router = new Router({ root: "/", name: "example", doc: "An example router" });
+
+server.use(new Scribe());
 
 server.use(router);
 
@@ -21,7 +23,7 @@ router.route({
 });
 
 server.use(unhandled());
-server.use(scribe());
+
 server.use(iterable());
 
 console.log("Server Running on 9090....");
--- src/plugins/scribe.ts
+++ src/plugins/scribe.ts
@@ -1,26 +1,38 @@
 import { Viae } from '../viae';
+import { RequestContext } from '../context';
 import { Method } from '../method';
 
-export function scribe() {
-  return {
-    plugin(viae: Viae) {
-      viae.before((ctx) => {
-        ctx["$start"] = process.hrtime();
-      });
-      viae.after((ctx) => {
-        if (ctx.req === undefined) return;
+export type ScribeContext = RequestContext & {
+  $start: number;
+  $span: number;
+}
 
-        let start = ctx["$start"];
+export class Scribe {
+  constructor(private log =
+    function (ctx: ScribeContext) {
+      if (ctx.req == undefined) return;
 
-        let method = ctx.req.method;
-        let path = ctx.req.path;
-        let status = ctx.res.status;
+      let method = ctx.req.method;
+      let path = ctx.req.path;
+      let status = ctx.res.status;
+      let span = ctx.$span;
 
-        let hrtime = process.hrtime(ctx["$start"]);
-        let span = hrtime[1] / 1000000;
-
-        console.log(Method[method], path, status, span + "ms" );
-      });
+      console.log(Method[method], path, status, span + "ms");
+    }) {
     }
+
+  plugin(viae: Viae) {
+    viae.before((ctx) => {
+      ctx["$start"] = process.hrtime();
+    });
+    viae.after((ctx) => {
+      let start = ctx["$start"];
+      let hrtime = process.hrtime(ctx["$start"]);
+      ctx["$span"] = hrtime[1] / 1000000;
+
+      this.log(ctx);
+    });
   }
-};
+}
+
+
