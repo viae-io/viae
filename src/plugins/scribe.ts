73a1f1bf1d3bd6ff7073d8a622de22405579b19b
Meirion Hughes <crakinshot@yahoo.com>
2017-07-12 22:27:30 +0100

    refactor: scribe context

--- src/plugins/scribe.ts
+++ src/plugins/scribe.ts
@@ -1,15 +1,21 @@
 import { Viae } from '../viae';
-import { ResponseContext } from '../context';
+import { Context } from '../context';
 import { Method } from '../method';
 import { Request } from '../request';
 
-export interface ScribePostContext extends ResponseContext {
+export interface ScribePostContext extends Context {
   /* start time (hrtime) */
   $start: [number, number];
+
+  /* milliseconds taken to process */
   $span: number;
 
-  /* the request header */
+  /* the request  */
   req?: Request;
+  
+  /* the response */
+  res?: Response;
+
   /* the path parameters extracted from the request */
   params?: any;
 }
@@ -35,7 +41,7 @@ export class Scribe {
     viae.after((ctx) => {
       let start = ctx["$start"];
       let end = process.hrtime(ctx["$start"]);
-      ctx["$span"] = end[1] /1000000;
+      ctx["$span"] = end[1] / 1000000;
 
       this.log(ctx);
     });
